b40d51b706135519b1acb5af847b8b7888f6dd76
Camila <mkp.camila@gmail.com>
2021-09-23 11:24:52 +0200

    Add capitalizeString(string); function

--- src/index.js
+++ src/index.js
@@ -38,11 +38,41 @@ function createRandomNumber(minNumber, maxNumber)
     //wir erschaffen eine zufallszahl aus einer minimalen und einer maximalen zahl
     const num = Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
 
+    // random = 0.5 * 50000 - 10000 = 15000 + 10000 = 2500
+
     //wir geben die zufällige nummer zurück
     return num;
 }
 
 
 // - Eine Funktion mit der wir den ersten Buchstaben des jeweiligen Wortes groß schreiben (PascalCase);
+
+/**
+ * @function capitalizeString
+ * @description Takes the first letter of a string and changes it to Uppercase, then returns string
+ * @param { string } word 
+ * @returns { string }
+ */
+function capitalizeString(word)
+{
+    //sicherheitsprüfung -> prüfe ob kein wort vorhanden ist
+    if(word === undefined || word.length === 0 || !isNaN(word))
+    {
+        return 'Default';
+    }
+
+    // wir nehmen de ersten buchstaben von unserem String, und ändern in auf UpperCase
+    const wordStart = word.charAt(0).toUpperCase();
+
+    // wir nehmen den rest des strings und ändern ihn auf LowerCase
+    const wordRest = word.substring(1).toLowerCase();
+
+    //wir geben den fertigen STring zurück
+    return wordStart + wordRest;
+
+}
+
+console.log(capitalizeString('hallo'));
+
 // - Eine Funktion mi der wie den benutzernamen generieren und auf die ergebnisse der anderen funktionen zugreifen;
 // - Einen export, um die Applikation in einer anderen Datei zu nutzen.
